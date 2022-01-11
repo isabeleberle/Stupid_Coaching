016d5191f2390e43ff036eaf059c4c0867bf0664
Isabel Eberle <i.eberle926@googlemail.com>
2022-01-11 19:24:49 +0100

    stupid coaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,3 @@
-QUESTION_WORK = "I am going to work"
-ANSWER_WORK = "Great!"
-ANSWER_QUESTION = "Silly question, get dressed and go to work!"
-ANSWER_ELSE = "I don't care, get dressed and go to work!"
-
 class QuestionsController < ApplicationController
     def ask
     end
@@ -11,14 +6,5 @@ class QuestionsController < ApplicationController
         @question = params[:question]
         @answer = stupid_coach(@question)
     end
-
-    private
-
-    def stupid_coach(str)
-        is_question?(str) ? ANSWER_QUESTION : ((str === QUESTION_WORK) ? ANSWER_WORK : ANSWER_ELSE)
-    end 
-
-    def is_question?(str)
-        (str === nil) ? false : str.end_with?("?")
-    end
+    
 end
